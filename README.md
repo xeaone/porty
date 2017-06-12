3c36a500d9beb0c82cc0bee3b1a8f9dd8e2d0dd1
Alexander Elias <alex.steven.elias@gmail.com>
2017-06-12 11:37:24 -0700

    fixed unhandled promises

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ Porty quickly and easily find available ports.
 Porty.find({
 	min: 8080,
 	max: 8090,
-	avoids: [8080, 8081, 8082, 8083, 8084]
+	avoids: [8081, 8080, 8082, 8083, 8084]
 }).then(function (port) {
 	console.log(`${port}`); // 8085
 }).catch(function (error) {
--- test.js
+++ test.js
@@ -3,15 +3,24 @@ const Porty = require('./index.js');
 Porty.find({
 	min: 8080,
 	max: 8090,
-	avoids: [8080, 8081, 8082, 8083, 8084]
+	avoids: [8081, 8080, 8082, 8083, 8084]
 }).then(function (port) {
 	console.log(`1st: ${port}`);
 }).catch(function (error) {
-	throw error;
+	console.log(error);
 });
 
 Porty.get().then(function (port) {
 	console.log(`2nd: ${port}`);
 }).catch(function (error) {
-	throw error;
+	console.log(error);
+});
+
+Porty.find({
+	min: 8002,
+	max: 8001
+}).then(function (port) {
+	console.log(`3rd: ${port}`);
+}).catch(function (error) {
+	console.log(error);
 });
